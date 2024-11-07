s1gtrap/graphviz

commit 8f2afe047367866179b7814a29aa4286e30d238c

common point_init: use a less ambiguous sentinel for detecting unset attributes
Using `MAXDOUBLE` here was ambiguous because technically it is a legal value for
`width` and `height`. Using something out of the legal range results in a more
robust check.

lib/common/shapes.c
@@ -3054,15 +3054,19 @@ static void point_init(node_t * n)
     size_t i, j;
     double w, h;
 
+    // a value outside of the range of `width`/`height` that we can use to
+    // detect when attributes have not been set
+    static const double UNSET = -1.0;
+
     /* set width and height, and make them equal
      * if user has set weight or height, use it.
      * if both are set, use smallest.
      * if neither, use default
      */
-    w = late_double(n, N_width, MAXDOUBLE, 0.0);
-    h = late_double(n, N_height, MAXDOUBLE, 0.0);
+    w = late_double(n, N_width, UNSET, 0.0);
+    h = late_double(n, N_height, UNSET, 0.0);
     w = fmin(w, h);
-    if (is_exactly_equal(w, MAXDOUBLE) && is_exactly_equal(h, MAXDOUBLE)) // neither defined
+    if (is_exactly_equal(w, UNSET) && is_exactly_equal(h, UNSET)) // neither defined
 	ND_width(n) = ND_height(n) = DEF_POINT;
     else {
 	w = fmin(w, h);

commit 0ac09ded482d37e3565aa521c7875d5d7bee395f

common point_init: fix incorrect construction of (-1, -1) points
Certain input graphs would fail an assertion:

  Assertion failed: (r->boundary[i] <= r->boundary[NUMDIMS + i]), function
    RTreeInsert, file index.c, line 192.

Rectangle construction was failing because it was based on points that claimed
to have width and height -72.0. This was the result of a bug that was introduced
in 8f2afe047367866179b7814a29aa4286e30d238c. What that change failed to take
into account was that the prior default of `MAXDOUBLE` ensured that the follow
up `w = fmin(w, h)`, in the presence of an unset width but set height, clamped
the width to the height’s value.

This change effectively reverts 8f2afe047367866179b7814a29aa4286e30d238c,
accounting for `MAXDOUBLE` having been replaced with the ISO C standard
`DBL_MAX` in the interim.

Gitlab: fixes #2613
Reported-by: cobizobi

lib/common/shapes.c
@@ -15,6 +15,7 @@
 #include <cgraph/gv_math.h>
 #include <common/render.h>
 #include <common/htmltable.h>
+#include <float.h>
 #include <limits.h>
 #include <math.h>
 #include <stddef.h>
@@ -3125,19 +3126,16 @@ static void point_init(node_t * n)
     size_t i, j;
     double w, h;
 
-    // a value outside of the range of `width`/`height` that we can use to
-    // detect when attributes have not been set
-    static const double UNSET = -1.0;
-
     /* set width and height, and make them equal
      * if user has set weight or height, use it.
      * if both are set, use smallest.
      * if neither, use default
      */
-    w = late_double(n, N_width, UNSET, MIN_NODEWIDTH);
-    h = late_double(n, N_height, UNSET, MIN_NODEHEIGHT);
+    w = late_double(n, N_width, DBL_MAX, MIN_NODEWIDTH);
+    h = late_double(n, N_height, DBL_MAX, MIN_NODEHEIGHT);
     w = fmin(w, h);
-    if (is_exactly_equal(w, UNSET) && is_exactly_equal(h, UNSET)) // neither defined
+    if (is_exactly_equal(w, DBL_MAX) &&
+        is_exactly_equal(h, DBL_MAX)) // neither defined
 	ND_width(n) = ND_height(n) = DEF_POINT;
     else {
 	w = fmin(w, h);

tests/test_regression.py
@@ -4692,10 +4692,6 @@ def test_2609(tmp_path: Path):
     pytest.fail("generated GIF was a solid color")
 
 
-@pytest.mark.xfail(
-    strict=re.search(r"\brelease\b", os.environ.get("CI_JOB_NAME", "").lower()) is None,
-    reason="https://gitlab.com/graphviz/graphviz/-/issues/2613",
-)
 def test_2613():
     """
     Graphviz should not fail an assertion when processing this graph