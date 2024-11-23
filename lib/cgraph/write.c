s1gtrap/graphviz

commit 3b81ca48fefe1bdcf30cbd3615ebf6dad60fb1db

fix double escaping during canonicalization
Escape sequences that were present in strings like node labels were incorrectly
re-escaped during canonicalization. The effect of this was that, when using
Graphviz programmatically, there was no way to add labels containing certain
characters. For example `"`.

The change in this commit recognizes escape sequences and avoids re-escaping
them. This is not ideal, but seems the best we can do with ambiguous input (it
is not strictly required for the input to be either escaped or unescaped).
Contradicting the official documentation,¹ this change recognizes `\"` as an
escape sequence for `"`. Without this, I see no way to represent `"` within a
label.

This test still needs to be xfailed on Windows because for some reason the test
binary cannot be compiled and linked.

Gitlab: #165, fixes #2397
Reported-by: Nikolai Kondrashov

¹ In particular, this sentence from
  https://graphviz.org/docs/attr-types/escString/, “A backslash appearing before
  any character not listed above is ignored.”

lib/cgraph/write.c
@@ -8,6 +8,7 @@
  * Contributors: Details at https://graphviz.org
  *************************************************************************/
 
+#include <assert.h>
 #include <limits.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -50,6 +51,43 @@ static bool is_id_char(char c) {
   return isalnum(c) || c == '.' || c == '-' || !isascii(c);
 }
 
+// is the prefix of this string a recognized Graphviz escape sequence?
+// https://graphviz.org/docs/attr-types/escString/
+static bool is_escape(const char *str) {
+  assert(str != NULL);
+
+  if (*str != '\\')
+    return false;
+
+  if (str[1] == 'E')
+    return true;
+  if (str[1] == 'G')
+    return true;
+  if (str[1] == 'H')
+    return true;
+  if (str[1] == 'L')
+    return true;
+  if (str[1] == 'N')
+    return true;
+  if (str[1] == 'T')
+    return true;
+
+  if (str[1] == 'l')
+    return true;
+  if (str[1] == 'n')
+    return true;
+  if (str[1] == 'r')
+    return true;
+
+  if (str[1] == '\\')
+    return true;
+
+  if (str[1] == '"')
+    return true;
+
+  return false;
+}
+
 /* _agstrcanon:
  * Canonicalize ordinary strings. 
  * Assumes buf is large enough to hold output.
@@ -60,6 +98,7 @@ static char *_agstrcanon(char *arg, char *buf)
     char uc;
     int cnt = 0, dotcnt = 0;
     bool needs_quotes = false;
+    bool part_of_escape = false;
     bool maybe_num;
     bool backslash_pending = false;
     static const char *tokenlist[]	/* must agree with scan.l */
@@ -76,11 +115,13 @@ static char *_agstrcanon(char *arg, char *buf)
     uc = *s++;
     maybe_num = isdigit(uc) != 0 || uc == '.' || uc == '-';
     while (uc) {
-	if (uc == '\"') {
+	if (uc == '\"' && !part_of_escape) {
 	    *p++ = '\\';
 	    needs_quotes = true;
-	} 
-	else if (maybe_num) {
+	} else if (!part_of_escape && is_escape(&s[-1])) {
+	    needs_quotes = true;
+	    part_of_escape = true;
+	} else if (maybe_num) {
 	    if (uc == '-') {
 		if (cnt) {
 		    maybe_num = false;
@@ -97,9 +138,12 @@ static char *_agstrcanon(char *arg, char *buf)
 		maybe_num = false;
 		needs_quotes = true;
 	    }
+	    part_of_escape = false;
 	}
-	else if (!(isalnum(uc) || uc == '_' || !isascii(uc)))
+	else if (!(isalnum(uc) || uc == '_' || !isascii(uc))) {
 	    needs_quotes = true;
+	    part_of_escape = false;
+	}
 	*p++ = uc;
 	uc = *s++;
 	cnt++;

tests/test_regression.py
@@ -29,6 +29,7 @@ from gvtest import (  # pylint: disable=wrong-import-position
     ROOT,
     dot,
     gvpr,
+    is_mingw,
     remove_xtype_warnings,
     run_c,
     which,
@@ -2788,7 +2789,11 @@ def test_2391_1():
     dot("svg", input)
 
 
-@pytest.mark.xfail(strict=True)  # FIXME
+@pytest.mark.xfail(
+    platform.system() == "Windows" and not is_mingw(),
+    reason="cannot link Agdirected on Windows",
+    strict=True,
+)  # FIXME
 def test_2397():
     """
     escapes in strings should be handled correctly

commit b83bd98d73a7a7c87723c38ec9cf645d1bcc020e

cgraph _agstrcanon: fix missed escaping of double quote characters
When a double quote followed an escape sequence (e.g. “foo\n"bar”), the state
machine in this function would incorrectly conclude the quote had already been
escaped and did not need a “\” prepended. The problem stemmed from the escape
character itself (“n” in the example previously given) not causing the
`part_of_escape` flag to be reset during its own processing. This bug was
introduced in 3b81ca48fefe1bdcf30cbd3615ebf6dad60fb1db.

The state machine in this function seems unnecessarily complex for what it is
doing. And it is not obviously correct, as evidenced by both
3b81ca48fefe1bdcf30cbd3615ebf6dad60fb1db and this fix. It may be wise to rewrite
this into something simpler in future.

Gitlab: fixes #2614
Reported-by: Thomas Braun

lib/cgraph/write.c
@@ -150,6 +150,8 @@ static char *_agstrcanon(char *arg, char *buf)
 	else if (!(gv_isalnum(uc) || uc == '_' || !isascii(uc))) {
 	    needs_quotes = true;
 	    part_of_escape = false;
+	} else {
+	    part_of_escape = false;
 	}
 	*p++ = uc;
 	uc = *s++;

tests/test_regression.py
@@ -4706,9 +4706,6 @@ def test_2613():
     dot("pdf", input)
 
 
-@pytest.mark.xfail(
-    strict=True, reason="https://gitlab.com/graphviz/graphviz/-/issues/2614"
-)
 def test_2614():
     """
     quotes in strings should be correctly escaped