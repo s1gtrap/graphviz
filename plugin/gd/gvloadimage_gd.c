s1gtrap/graphviz

commit 3677b2efb4dced4ec1befea640e4bc9860b2173b

gd plugin gd_loadimage_cairo: fix memory leak of 'data'

plugin/gd/gvloadimage_gd.c
@@ -167,6 +167,7 @@ static void gd_loadimage_cairo(GVJ_t * job, usershape_t *us, boxf b, bool filled
         cairo_restore(cr);
 
 	cairo_surface_destroy(surface);
+	free(data);
     }
 }
 #endif

commit 152550d1f8ab6d99924629106842cb50147b16b2

GD plugin gd_loadimage_cairo: fix 'free' of invalid pointer
The `data` pointer is incremented within this function. So passing it to `free`
was not passing the originally allocated address but rather some offset into the
allocated memory. This bug was introduced in
3677b2efb4dced4ec1befea640e4bc9860b2173b.

It is unclear to me why this was not caught in CI. The bug did not appear to
cause any crashes, even with input that certainly exercises this code path. The
bug was discovered by running various parts of the test suite locally with ASan
enabled.

Gitlab: #2619
Reported-by: Address Sanitizer
Reported-by: Ely Schoenfeld

plugin/gd/gvloadimage_gd.c
@@ -123,6 +123,7 @@ static void gd_loadimage_cairo(GVJ_t * job, usershape_t *us, boxf b, bool filled
 	unsigned char *data = gv_calloc((size_t)stride, (size_t)height);
 	surface = cairo_image_surface_create_for_data (data, CAIRO_FORMAT_ARGB32,
 							width, height, stride);
+	unsigned char *const orig_data = data;
 
 	if (im->trueColor) {
 	    if (im->saveAlphaFlag) {
@@ -169,7 +170,7 @@ static void gd_loadimage_cairo(GVJ_t * job, usershape_t *us, boxf b, bool filled
         cairo_restore(cr);
 
 	cairo_surface_destroy(surface);
-	free(data);
+	free(orig_data);
     }
 }
 #endif