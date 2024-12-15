s1gtrap/graphviz

commit 8a67bf0ddabd9152fe0caa27639bd5b1d7c00a4d

CMake: fix 'pango_fc_font_lock_face' discovery
8bb838d4dc3bbfd77c32050aa65f8f89bfd52c98 discovered the symbol availability but
then never propagated this information to C code. This is still disabled on
Windows (including Cygwin and MinGW) where these symbols are apparently not
available.

Gitlab: #2530

cmake/config_checks.cmake
@@ -16,7 +16,10 @@ check_function_exists( drand48          HAVE_DRAND48         )
 check_function_exists( inotify_init1    HAVE_INOTIFY_INIT1   )
 check_function_exists( lrand48          HAVE_LRAND48         )
 check_function_exists( memrchr          HAVE_MEMRCHR         )
-if(PANGOCAIRO_FOUND AND ${PANGOCAIRO_VERSION} VERSION_GREATER_EQUAL 1.4)
+if(PANGOCAIRO_FOUND AND
+   ${PANGOCAIRO_VERSION} VERSION_GREATER_EQUAL 1.4 AND
+   NOT CYGWIN AND
+   NOT WIN32)
   message(STATUS "Pangocairo >= 1.4, so have pango_fc_font_lock_face")
   set(HAVE_PANGO_FC_FONT_LOCK_FACE 1)
 else()

config-cmake.h.in
@@ -12,6 +12,7 @@
 #cmakedefine HAVE_INOTIFY_INIT1
 #cmakedefine HAVE_LRAND48
 #cmakedefine HAVE_MEMRCHR
+#cmakedefine HAVE_PANGO_FC_FONT_LOCK_FACE
 #cmakedefine HAVE_SETENV
 #cmakedefine HAVE_SETMODE
 #cmakedefine HAVE_SRAND48

commit 0036a9f1ca9ecdf6ec7cf4a0c993b2034a847926

CMake: fix: disable 'pango_fc_font_lock_face' discovery on non-Linux
As fallout from 8a67bf0ddabd9152fe0caa27639bd5b1d7c00a4d, macOS builds now fail
as these symbols are not available. Given the caveats with this discovery, lets
conservatively assume this is available nowhere except Linux.

Gitlab: fixes #2628

cmake/config_checks.cmake
@@ -18,8 +18,7 @@ check_function_exists( lrand48          HAVE_LRAND48         )
 check_function_exists( memrchr          HAVE_MEMRCHR         )
 if(PANGOCAIRO_FOUND AND
    ${PANGOCAIRO_VERSION} VERSION_GREATER_EQUAL 1.4 AND
-   NOT CYGWIN AND
-   NOT WIN32)
+   ${CMAKE_SYSTEM_NAME} MATCHES "Linux")
   message(STATUS "Pangocairo >= 1.4, so have pango_fc_font_lock_face")
   set(HAVE_PANGO_FC_FONT_LOCK_FACE 1)
 else()