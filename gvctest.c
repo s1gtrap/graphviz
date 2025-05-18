cdef9f9b64ff97af7cedbcde68694e3b0446e809
William Tange <will@tan.ge>
2025-05-19 00:39:43 +0200

    Try to render the graph

--- gvctest.c
+++ gvctest.c
@@ -1,8 +1,16 @@
+#include "lib/gvc/gvc.h"
 #include <cgraph.h>
+#include <gvc.h>
 #include <stdio.h>
 
 int main() {
+  GVC_t *gvc;
   Agraph_t *g;
+
+  gvc = gvContext();
   g = agopen("G", Agdirected, NULL);
+
+  gvRender(gvc, g, "dot", stdout);
+
   printf("Hello, world!\n");
 }
